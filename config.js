df2e9d75cd8490dbe2f9fc907a2e2e6134c7115a
pasantehseq1927-create <pasantehseq1927@gmail.com>
2026-08-11 08:33:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
   El modo de prueba nunca envía información, aunque esta URL esté configurada.
 */
 const APP_CONFIG = {
-  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwYtE8FHmzaC1AIY-HjQQRWKUgXt7W3vpYXG8scf46TGSnnPiYH5Zzn8BFyIEUuhcAvaA/exec",
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwYDuj8TLlMJ5-5WVNftxASKnAQDsdxhJOusS3Ld7z-Ez6GkgYAXj1kHujH1ipcoTRjjg/exec",
   ORGANIZATION_NAME: "RMS",
   DEMO_ENABLED: false,
   APP_EDITION: "FINAL"
